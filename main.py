e8e629ef8ad0d07d83fb6fc3369f56ecb061726f
researcher2312 <researcher2312@gmail.com>
2024-05-06 23:27:16 +0200

    added socket error catching

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import asyncio
 import socketpool
 
 
-
 class SocketManager:
     def __init__(self, connection_manager):
         self.connection_manager = connection_manager
@@ -13,23 +12,30 @@ class SocketManager:
         self.socket_initialised = False
 
     def create_socket(self):
-        self.socket = self.pool.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.bind((self.device_address, 23))
+        self.socket = self.pool.socket(self.pool.AF_INET, self.pool.SOCK_STREAM)
+        address = str(self.connection_manager.device_address)
+        self.socket.bind((address, 1234))
+        self.socket.settimeout(5)
+        self.socket_initialised = True
 
     async def send_data(self):
-        if self.connection_manager.device_connected:
-            if not self.socket_initialised:
-                pass
-            else:
-                pass
-        await asyncio.sleep(10)
+        while True:
+            if self.connection_manager.device_connected:
+                if not self.socket_initialised:
+                    self.create_socket()
+                else:
+                    try:
+                        self.socket.send(b"testiiing")
+                    except BrokenPipeError:
+                        print("send error")
+            await asyncio.sleep(10)
 
 class ConnectionManager:
     def __init__(self):
         self.device_connected = False
         self.device_address = None
         wifi.radio.start_ap(ssid="Astronaut", password="12345678")
-        wifi.radio.start_dhcp_ap()
+        #wifi.radio.start_dhcp_ap()
 
     def get_connected_ip(self):
         stations = wifi.radio.stations_ap
@@ -41,17 +47,19 @@ class ConnectionManager:
         else:
             self.device_connected = False
 
-    async def monitor_connections(self):
+    def monitor_connections(self):
         while True:
             self.get_connected_ip()
-            print(self.device_connected)
+            print(self.device_address)
             await asyncio.sleep(1)
 
+
 async def main():
-    print("test")
     connection_manager = ConnectionManager()
     socket_manager = SocketManager(connection_manager)
-    asyncio.create_task(connection_manager.monitor_connections())
-    asyncio.create_task(socket_manager.send_data())
+    monitor_task = asyncio.create_task(connection_manager.monitor_connections())
+    sender_task = asyncio.create_task(socket_manager.send_data())
+    await asyncio.gather(monitor_task)
+
 
 asyncio.run(main())
