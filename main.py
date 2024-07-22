d60be953dcc483a433426af2512e8d8a8091f7cc
researcher2312 <researcher2312@gmail.com>
2024-07-22 23:41:16 +0200

    add: neopixel library

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import asio
 from sensor_reader import DataReader
 from network_sender import ConnectionManager, SocketManager
+from lights import Lights
 
 
 async def main():
     connection_manager = ConnectionManager()
     socket_manager = SocketManager(connection_manager)
+    lights = Lights()
     monitor_task = asyncio.create_task(connection_manager.monitor_connections())
     sender_task = asyncio.create_task(socket_manager.send_data())
+    lights.light_up()
     await asyncio.gather(monitor_task)
 
 
