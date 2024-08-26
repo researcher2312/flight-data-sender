376d22293d307938eac12ac8efe445ecf7e81e19
researcher2312 <researcher2312@gmail.com>
2024-08-27 00:23:03 +0200

    add: BNO055 sensor code

--- lm75.py
+++ lm75.py
@@ -1,9 +1,10 @@
 from adafruit_bus_device.i2c_device import I2CDevice
 
+ADDRESS = 0x48
+
 class LM75:
-    ADDRESS = 0x48
     def __init__(self, i2c):
-        self._device = I2CDevice(i2c,address)
+        self._device = I2CDevice(i2c,ADDRESS)
         
     def _twos_comp(self, val, bits):
         if (val & (1 << (bits - 1))):
@@ -16,7 +17,7 @@ class LM75:
             self._device.readinto(buf)
         return buf
 
-    def temperature(self):
+    def get_temperature(self):
         [t_high,t_low] = self._get_temp_bytes()
         fraction = 0.5*(t_low >> 7)
         return self._twos_comp(t_high,8) + fraction
--- sensor_reader.py
+++ sensor_reader.py
@@ -1,20 +1,24 @@
 import board
 import busio
 import asyncio
-from bmo055 import BMO055 as bmo
+from bno_055 import BNO055
 from lm75 import LM75
 
-        
 class DataReader:
     def __init__(self):
         self.i2c = busio.I2C(board.GP27, board.GP26)
         self.thermometer = LM75(self.i2c)
-        self.imu = bmo(self.i2c)
-        
+        self.imu = BNO055(self.i2c)
+        self.temperature = 0
+
     def read_data(self):
-        print(self.thermometer.temperature())
-    
+        try:
+            self.temperature = self.thermometer.get_temperature()
+        except OSError:
+            print("read error")
+
     async def run_reading(self):
         while True:
             self.read_data()
+            print(self.temperature)
             await asyncio.sleep(1)
