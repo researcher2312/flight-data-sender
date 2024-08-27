fa493253fe2b1e87ebb59e54df6bdda17d9c4b59
researcher2312 <researcher2312@gmail.com>
2024-08-27 20:02:22 +0200

    fix: rename methodes

--- code.py
+++ code.py
@@ -9,10 +9,10 @@ async def main():
     socket_manager = SocketManager(connection_manager)
     reader = DataReader()
     lights = Lights()
-    monitor_task = asyncio.create_task(connection_manager.monitor_connections())
-    sender_task = asyncio.create_task(socket_manager.send_data())
+    monitor_task = asyncio.create_task(connection_manager.monitor_connections_task())
+    sender_task = asyncio.create_task(socket_manager.send_task(reader))
     lights_task = asyncio.create_task(lights.blink())
-    sensor_task = asyncio.create_task(reader.run_reading())
+    sensor_task = asyncio.create_task(reader.reading_task())
     await asyncio.gather(monitor_task, sensor_task)
 
 
--- sensor_reader.py
+++ sensor_reader.py
@@ -11,15 +11,17 @@ class DataReader:
         self.thermometer = LM75(self.i2c)
         self.imu = BNO055(self.i2c)
         self.temperature = 0
+        self.acceleration = [1.2, 3.4, 5.6]
+        self.rotation = [7.8, 9.0, 12.3]
+        self.magnetic = [4.56, 7.89, 0.12]
 
-    def read_data(self):
+    def read_temperature(self):
         try:
             self.temperature = self.thermometer.get_temperature()
         except OSError:
             print("read error")
 
-    async def run_reading(self):
+    async def reading_task(self):
         while True:
-            self.read_data()
-            print(self.temperature)
+            self.read_temperature()
             await asyncio.sleep(1)
